256e4c71cb74e4e0c60f6da4c00b4510a2c4ff54
lucas-haupt <81650231+lucas-haupt@users.noreply.github.com>
2021-12-07 12:02:30 -0600

    First iteration of simulation

--- queries/division_data.sql
+++ queries/division_data.sql
@@ -5,3 +5,5 @@ S.league_id AS "league_id",
 S.conference_id AS "conference_id",
 S.division_id AS "division_id"
 FROM football.football_team_season S
+WHERE s.league_id = 8
+AND CAST(substr(season_id, 1, 4) AS INT) >= 2015
--- queries/game_data.sql
+++ queries/game_data.sql
@@ -2,11 +2,11 @@ select
     GAME_CODE as "game_code",
     GAME_DATE as "game_date",
     HOME_TEAM_ID as "home_team_id",
-    VIS_TEAM_ID as "vis_team_id",
+    VIS_TEAM_ID as "away_team_id",
     SEASON as "season",
     HOME_TEAM_ABBREV as "home_team_abbrev",
-    VIS_TEAM_ABBREV as "vis_team_abbrev",
+    VIS_TEAM_ABBREV as "away_team_abbrev",
     WEEK as "week",
     HOME_SCORE as "home_score",
-    VIS_SCORE as "vis_score"
+    VIS_SCORE as "away_score"
 from customer_data.cd_football_game
