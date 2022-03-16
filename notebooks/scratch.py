c63f126560e2024c9108672a8353c35f090fa136
lucas-haupt <81650231+lucas-haupt@users.noreply.github.com>
2022-03-16 16:12:18 -0500

    Updates for things

--- notebooks/scratch.py
+++ notebooks/scratch.py
@@ -1,3 +1,9 @@
-big_cv = GroupKFold(n_splits=5)
-final_model = cb.CatBoostClassifier(n_estimators=search_game_outcome.best_estimator_.tree_count_,**search_game_outcome.best_params_,cat_features=categoricals,verbose=1)
-predictions_game_outcome = cross_val_predict(final_model,X_all, y_all_game_outcome,groups=group_all_game_outcome,cv=big_cv,verbose=1,n_jobs=1, method="predict_proba")
+def create_train_test_val_df(
+    df,
+    input_names,
+    output_name,
+    mask_train=(event_df.season <= 2019) & (event_df.continuation == 0),
+    mask_test=(event_df.season == 2021) & (event_df.continuation == 0),
+    mask_val=(event_df.season == 2020) & (event_df.continuation == 0),
+):
+    X_train = 0
