e3ea1045d9301a9b1ba99168e596fde1c033e756
lucas-haupt <81650231+lucas-haupt@users.noreply.github.com>
2022-05-11 13:39:23 -0500

    newest model
    model includes weighted vegas points predictions

--- notebooks/scratch.py
+++ notebooks/scratch.py
@@ -1,24 +1,17 @@
-    elif model =="rf_search_rf_vegas_adjusted":
-        y = ["xhome_win_rf_search_clipped_rf_drive_preds_vegas_adjusted", "xaway_win_rf_search_clipped_rf_drive_preds_vegas_adjusted", "xovertime_rf_search_clipped_rf_drive_preds_vegas_adjusted"]
-        y1 = "xend_of_regulation_score_diff_rf_search_clipped_rf_drive_preds_vegas_adjusted"
-        y2 = "xend_of_regulation_score_total_rf_search"
-        y3 = search_rf_drive_class_names
-        y4 = "search_rf_play_first_down"
-    elif model =="rf_search_mlp_vegas_adjusted":
-        y = ["xhome_win_rf_search_clipped_mlp_drive_preds_vegas_adjusted", "xaway_win_rf_search_clipped_mlp_drive_preds_vegas_adjusted", "xovertime_rf_search_clipped_mlp_drive_preds_vegas_adjusted"]
-        y1 = "xend_of_regulation_score_diff_rf_search_clipped_mlp_drive_preds_vegas_adjusted"
-        y2 = "xend_of_regulation_score_total_rf_search"
-        y3 = search_mlp_drive_class_names
-        y4 = "search_mlp_play_first_down"
-    elif model =="mlp_search_rf_vegas_adjusted":
-        y = ["xhome_win_mlp_search_clipped_rf_drive_preds_vegas_adjusted", "xaway_win_mlp_search_clipped_rf_drive_preds_vegas_adjusted", "xovertime_mlp_search_clipped_rf_drive_preds_vegas_adjusted"]
-        y1 = "xend_of_regulation_score_diff_mlp_search_clipped_rf_drive_preds_vegas_adjusted"
-        y2 = "xend_of_regulation_score_total_mlp_search"
-        y3 = search_rf_drive_class_names
-        y4 = "search_rf_play_first_down"
-    elif model =="mlp_search_mlp_vegas_adjusted":
-        y = ["xhome_win_mlp_search_clipped_mlp_drive_preds_vegas_adjusted", "xaway_win_mlp_search_clipped_mlp_drive_preds_vegas_adjusted", "xovertime_mlp_search_clipped_mlp_drive_preds_vegas_adjusted"]
-        y1 = "xend_of_regulation_score_diff_mlp_search_clipped_mlp_drive_preds_vegas_adjusted"
-        y2 = "xend_of_regulation_score_total_mlp_search"
-        y3 = search_mlp_drive_class_names
-        y4 = "search_mlp_play_first_down"
+model_df["home_team_win_in_regulation"] = np.where(model_df["end_of_regulation_score_diff"]>0, 1, 0)
+model_df["away_team_win_in_regulation"] = np.where(model_df["end_of_regulation_score_diff"]<0, 1, 0)
+model_df["tie_at_end_of_regulation"] = np.where(model_df["end_of_regulation_score_diff"]==0, 1, 0)
+calb_df = model_df[(model_df["season"]==2020)&~(model_df.xhome_win_mlp_search_clipped_rf_drive_preds_vegas_adjusted.isna())]
+# calb = calibration.calibration_curve(calb_df["home_team_win"], np.clip(calb_df["xhome_win_score_diff_mlp"], 0, 1), n_bins=10, strategy="quantile")
+# plt.plot(calb[1], calb[0], marker="o")
+bins = 20
+
+calb = calibration.calibration_curve(calb_df["home_team_win_in_regulation"], np.clip(calb_df["xhome_win_mlp_search_clipped_rf_drive_preds_vegas_adjusted"], 0, 1), n_bins=bins, strategy="quantile")
+plt.plot(calb[1], calb[0], marker="o")
+calb = calibration.calibration_curve(calb_df["home_team_win_in_regulation"], np.clip(calb_df["xhome_win_mlp_search_clipped_mlp_drive_preds_vegas_adjusted"], 0, 1), n_bins=bins, strategy="quantile")
+plt.plot(calb[1], calb[0], marker="o")
+calb = calibration.calibration_curve(calb_df["home_team_win_in_regulation"], np.clip(calb_df["xhome_win_rf_search_clipped_rf_drive_preds_vegas_adjusted"], 0, 1), n_bins=bins, strategy="quantile")
+plt.plot(calb[1], calb[0], marker="o")
+calb = calibration.calibration_curve(calb_df["home_team_win_in_regulation"], np.clip(calb_df["xhome_win_rf_search_clipped_mlp_drive_preds_vegas_adjusted"], 0, 1), n_bins=bins, strategy="quantile")
+plt.plot(calb[1], calb[0], marker="o")
+plt.plot([0, 1], [0, 1])
