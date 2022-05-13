f9cffd7a1a76d9232a1fb3888542b99838a2a419
lucas-haupt <81650231+lucas-haupt@users.noreply.github.com>
2022-05-13 13:47:52 -0500

    Updated models

--- notebooks/scratch.py
+++ notebooks/scratch.py
@@ -1,17 +1,4 @@
-model_df["home_team_win_in_regulation"] = np.where(model_df["end_of_regulation_score_diff"]>0, 1, 0)
-model_df["away_team_win_in_regulation"] = np.where(model_df["end_of_regulation_score_diff"]<0, 1, 0)
-model_df["tie_at_end_of_regulation"] = np.where(model_df["end_of_regulation_score_diff"]==0, 1, 0)
-calb_df = model_df[(model_df["season"]==2020)&~(model_df.xhome_win_mlp_search_clipped_rf_drive_preds_vegas_adjusted.isna())]
-# calb = calibration.calibration_curve(calb_df["home_team_win"], np.clip(calb_df["xhome_win_score_diff_mlp"], 0, 1), n_bins=10, strategy="quantile")
-# plt.plot(calb[1], calb[0], marker="o")
-bins = 20
-
-calb = calibration.calibration_curve(calb_df["home_team_win_in_regulation"], np.clip(calb_df["xhome_win_mlp_search_clipped_rf_drive_preds_vegas_adjusted"], 0, 1), n_bins=bins, strategy="quantile")
-plt.plot(calb[1], calb[0], marker="o")
-calb = calibration.calibration_curve(calb_df["home_team_win_in_regulation"], np.clip(calb_df["xhome_win_mlp_search_clipped_mlp_drive_preds_vegas_adjusted"], 0, 1), n_bins=bins, strategy="quantile")
-plt.plot(calb[1], calb[0], marker="o")
-calb = calibration.calibration_curve(calb_df["home_team_win_in_regulation"], np.clip(calb_df["xhome_win_rf_search_clipped_rf_drive_preds_vegas_adjusted"], 0, 1), n_bins=bins, strategy="quantile")
-plt.plot(calb[1], calb[0], marker="o")
-calb = calibration.calibration_curve(calb_df["home_team_win_in_regulation"], np.clip(calb_df["xhome_win_rf_search_clipped_mlp_drive_preds_vegas_adjusted"], 0, 1), n_bins=bins, strategy="quantile")
-plt.plot(calb[1], calb[0], marker="o")
-plt.plot([0, 1], [0, 1])
+for key in search_mlp_score_diff_clipped_mlp_drive_preds.best_params_.keys():
+    rf_grid[key] = [search_rf_score_diff_clipped_mlp_drive_preds.best_params_[key]]
+for key in search_mlp_score_diff_clipped_mlp_drive_preds.best_params_.keys():
+    mlp_grid[key] = [search_mlp_score_diff_clipped_mlp_drive_preds.best_params_[key]]
---
+++ queries/odds_data_cfb.sql
@@ -0,0 +1,39 @@
+SELECT
+    G.game_code AS "game_code",
+    G.casino_id AS "casino_id",
+    G.casino_name AS "casino_name",
+    G.status AS "status",
+    g.status_desc AS "status_desc",
+    G.season AS "season",
+    G.game_type_desc AS "game_type_desc",
+    g.scope_id AS "scope_id",
+    G.game_date AS "game_date",
+    G.home_team_name || ' ' || home_team_nickname AS "home_team",
+    G.away_team_name || ' ' || away_team_nickname AS "away_team",
+    G.cur_favorite_id AS "cur_favorite_id",
+    CASE WHEN G.cur_favorite_id = 1 THEN G.cur_spread 
+        ELSE -G.cur_spread END AS "cur_spread",
+    G.cur_over_under AS "cur_over_under",
+    CASE WHEN G.cur_home_money_line <= -100 THEN 1 / (1-100 / G.cur_home_money_line) 
+        ELSE 1 / (1 + G.cur_home_money_line / 100) 
+        END AS "home_odds",
+    CASE WHEN G.cur_away_money_line <= -100 THEN 1 / (1 - 100 / G.cur_away_money_line) 
+        ELSE 1 / (1 + G.cur_away_money_line / 100) 
+        END AS "away_odds",
+    CASE WHEN G.cur_favorite_money_line <= -100 THEN 1 / (1-100 / G.cur_favorite_money_line) 
+        ELSE 1 / (1 + G.cur_favorite_money_line / 100) 
+        END AS "favorite_odds",
+    CASE WHEN G.cur_underdog_money_line <= -100 THEN 1 / (1-100 / G.cur_underdog_money_line) 
+        ELSE 1 / (1 + G.cur_underdog_money_line / 100) 
+        END AS "underdog_odds"
+
+FROM customer_data.cd_gaming_games G
+WHERE G.league_id = 16
+    AND G.game_type_id NOT IN (0, 5)
+    AND G.season >= 2012
+    AND G.cur_home_money_line IS NOT NULL
+    AND G.scope_id = 1
+    AND G.cur_home_money_line != 0
+    AND G.cur_away_money_line != 0
+    AND (g.status = 1 OR g.status = 4)
+    ORDER BY G.season, G.game_code, G.status desc
