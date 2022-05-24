4ba2a0de6ada26ab613d3748bf10606c9e5d08e8
lucas-haupt <81650231+lucas-haupt@users.noreply.github.com>
2022-05-24 10:44:48 -0500

    go for 4th bot

--- notebooks/scratch.py
+++ notebooks/scratch.py
@@ -1,4 +1,12 @@
-for key in search_mlp_score_diff_clipped_mlp_drive_preds.best_params_.keys():
-    rf_grid[key] = [search_rf_score_diff_clipped_mlp_drive_preds.best_params_[key]]
-for key in search_mlp_score_diff_clipped_mlp_drive_preds.best_params_.keys():
-    mlp_grid[key] = [search_mlp_score_diff_clipped_mlp_drive_preds.best_params_[key]]
+        if show_rf == True:
+            ax[0].plot(score_difference_list, pred_output, 'green', label='rf', linewidth=3, marker="o")
+        if show_log_basic == True:
+            ax[0].plot(score_difference_list, pred_output_basic_loglin, '#AABA00', label='log_basic', linewidth=3, marker="o")
+        if show_mlp_basic == True:
+            ax[0].plot(score_difference_list, pred_output_basic_mlp, '#A200BA', label='mlp_basic', linewidth=3, marker="o")
+        if show_mlp_vegas == True:
+            ax[0].plot(score_difference_list, pred_output_basic_mlp_vegas, '#A200BA', label='mlp_vegas', linewidth=3, marker="o")
+        if show_mlp_advanced == True:
+            ax[0].plot(score_difference_list, pred_output_advanced_mlp, 'blue', label='mlp_advanced', linewidth=3, marker="o")
+        if show_log_advanced == True:
+            ax[0].plot(score_difference_list, pred_output_advanced_log, '#009B9C', label='loglin_advanced', linewidth=3, marker="o")
